77922ccdf56c6b2ff56b95edb27f3267b817d506
edgarpascacioca <104470038+edgarpascacioca@users.noreply.github.com>
2022-05-18 08:47:35 -0400

    CRUD files

--- config.php
+++ config.php
@@ -1,30 +1,16 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-
-function databaseConnect(){
-	define('DB_SERVER','localhost');
-	define('DB_USERNAME','root');
-	define('DB_PASSWORD','root');
-	define('DB_NAME','testdb');
-
-	$link= mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-	if($link === false){
-		die("ERROR: Could not connect. " . mysqli_connect_error());
-	}
-	return $link;
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'testdb');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-function listEmployees(){
-	$link = databaseConnect();
-	$sql = "SELECT id, name, address,salary FROM employee";
-	$result = $link->query($sql);
-	$link->close();
-	return $result;
-}
-
 ?>
--- index.php
+++ index.php
@@ -1,49 +1,87 @@
-<?php require("config.php"); ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Database Design Group - Final Project - Employees List</title>
-	<style>
-		table, th, td {
-  			border: 1px solid black;
-		}
-	</style>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <style type="text/css">
+        .wrapper{
+            width: 650px;
+            margin: 0 auto;
+        }
+        .page-header h2{
+            margin-top: 0;
+        }
+        table tr td:last-child a{
+            margin-right: 15px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
 </head>
 <body>
-	<h1>Employees Details</h1>
-	<a href="create.php">Add New Employee</a>
-	<table >
-		<tr>
-			<th>#</th>
-			<th>Name</th>
-			<th>Address</th>
-			<th>Salary</th>
-			<th>Action</th>
-		</tr>
-		<?php
-		$employees = listEmployees();
-		if ($employees->num_rows > 0) {
-	  // output data of each row
-	  while($row = $employees->fetch_assoc()) {
-	  	?>
-	  	<tr>
-			<td><?php echo $row["id"]; ?></td>
-			<td><?php echo $row["name"]; ?></td>
-			<td><?php echo $row["address"]; ?></td>
-			<td><?php echo $row["salary"]; ?></td>
-			<td>
-				<a href="read.php?id=<?php echo $row["id"] ?>">View</a>
-				<a href="update.php?id=<?php echo $row["id"] ?>">Edit</a>
-				<a href="delete.php?id=<?php echo $row["id"] ?>">Delete</a>
-			</td>
-		</tr>
-	    
-	    <?php
-	  }
-	} else {
-	  echo "0 results";
-	}
-	 ?>
-		
-	</table>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM employee";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Name</th>";
+                                        echo "<th>Address</th>";
+                                        echo "<th>Salary</th>";
+                                        echo "<th>Action</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['address'] . "</td>";
+                                        echo "<td>" . $row['salary'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                            echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                    }
+ 
+                    // Close connection
+                    mysqli_close($link);
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
